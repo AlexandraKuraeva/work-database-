6e46cda8ea7f4710aa44a18b2539046f64e3c3ec
AlexandraKuraeva <119942678+AlexandraKuraeva@users.noreply.github.com>
2023-11-17 01:35:33 +0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	 
   </head>
   <body>
-    <h1>Форма добавления в очередь!!!!!!!!!!</h1>
+    <h1>Форма добавления в очередь</h1>
 
     <form 
 	 action="require.php"
@@ -20,8 +20,8 @@
     </form>
 	 
     <form action="export-pdf.php" method="post">
-		    <input id="generatePdfBtn" type="submit" name="exportPDF" value="Export"> 
-	 </form>
+	<input id="generatePdfBtn" type="submit" name="exportPDF" value="Export"> 
+    </form>
 
   </body>
 </html>
