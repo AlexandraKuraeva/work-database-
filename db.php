3ff07b8ee9d070b18042476bb14755e516c497ac
AlexandraKuraeva <alexa.kurav@yandex.ru>
2023-11-17 12:17:05 +0400

    Update db

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 include(' config.php');
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if(!$conn){
 	  die("Connection failed: " . mysqli_connect_error());
 }else{
